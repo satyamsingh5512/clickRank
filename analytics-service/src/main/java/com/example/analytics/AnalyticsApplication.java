67ad7c3f1a79b8882e0e279739a077c953aa6ff5
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-03-22 23:15:36 +0530

    add analytics service and docker-compose configuration

---
+++ analytics-service/src/main/java/com/example/analytics/AnalyticsApplication.java
@@ -0,0 +1,14 @@
+package com.example.analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+
+@SpringBootApplication
+@EnableKafkaStreams
+public class AnalyticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalyticsApplication.class, args);
+    }
+}
