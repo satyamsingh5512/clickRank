fc9a8f8430f811df3f8f97047b7be72f00da6acc
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-05-22 14:36:44 +0530

    feat: add MLRankingApplication as the main entry point for ML ranking service

---
+++ services/ml-ranking-service/src/main/java/com/example/ml/MLRankingApplication.java
@@ -0,0 +1,12 @@
+package com.example.ml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MLRankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MLRankingApplication.class, args);
+    }
+}
